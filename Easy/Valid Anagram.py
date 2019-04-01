a444f57866d9d12775d1c5cf958bcbd7efdf9e16
Raymond Lam <raylam007@gmail.com>
2019-04-01 16:10:06 -0700

    242. Valid Anagram

---
+++ Easy/Valid Anagram.py
@@ -0,0 +1,15 @@
+'''
+Given two strings s and t , write a function to determine if t is an anagram of s.
+Note:
+You may assume the string contains only lowercase alphabets.
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+
+https://leetcode.com/problems/valid-anagram/
+'''
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if s == None or t == None:
+            return False
+        return sorted(list(s)) == sorted(list(t))
