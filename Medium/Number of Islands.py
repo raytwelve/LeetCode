2823c98651c6430f38fdeec68be78278ce8d4d70
Raymond Lam <raylam007@gmail.com>
2019-04-11 21:48:06 -0700

    added doumentation for Number of Islands.py

--- Medium/Number of Islands.py
+++ Medium/Number of Islands.py
@@ -15,12 +15,15 @@ class Solution:
         y = len(grid[0])
         islands = 0
         land = "1"
-        lands = set()
+        lands = set() #every '1' on the whole graph
         for i in range(x):
             for j in range(y):
                 if grid[i][j] == land and (i,j) not in lands:
+                    # found a piece of land that we have not already discovered
+                    # its a new island!
                     thisIsland = set()
                     thisIsland.add((i,j))
+                    # lets explore!
                     self.findLands(thisIsland, lands, grid, x, y)
                     islands +=1
         return islands
@@ -28,17 +31,25 @@ class Solution:
     def findLands(self, thisIsland, lands, grid, x, y):
         land = "1"
         
+        # exploreNext will be the land we need to explore in the next loop iteration
         exploreNext = set(thisIsland)
+
         while len(exploreNext) > 0:
-#             cannot change set while iterating thru, create one to track 
+            # we want to iterate thru exploreNext, but we will also have a new set of lands to explore in the next iteration
+            # for x in exploreNext. find the lands to the up,down,left,right of it (we will need to explore these next)
+            # since we are iterating thru exploreNext, we cannot update it with the next iterations pieces of land.. we dont want to visit the land we have already visited in this iteration
+
+
             newLands = set(exploreNext)
             exploreNext = set()
             for (i, j) in newLands:
-                discoveredNewLands = set()
+                discoveredNewLands = set() #used within each iteration of this for loop
+                
+                # make copies to increment, then comeback to the original piece
                 ii = i
                 jj = j
 
-                ii = i+1
+                ii = i+1 #keep exploring to the south
                 while ii < x and grid[ii][jj] == land and (ii,jj) not in thisIsland:
                     discoveredNewLands.add((ii,jj))
                     ii += 1
@@ -47,7 +58,8 @@ class Solution:
                 while ii > -1 and grid[ii][jj] == land and (ii,jj) not in thisIsland:
                     discoveredNewLands.add((ii,jj))
                     ii += 1
-                ii = i
+                
+                ii = i #reset to original
 
                 jj = j+1
                 while jj < y and grid[ii][jj] == land and (ii,jj) not in thisIsland:
@@ -60,6 +72,14 @@ class Solution:
                     jj += 1
                 jj = j
                 
+
+                # for each piece of land we have, we will have a set of lands we discovered. we keep adding these to one big collection  'exploreNext'
                 exploreNext.update(discoveredNewLands)
+                
+                # thisIsland is all the pieces of land we find in this function call; aka summation of every set of discoveredNewLands... + the original piece of land we came into the function with
+                # this is used to make sure we dont check them again in the succeeding loop iterations
                 thisIsland.update(discoveredNewLands)
+
+        # weve finally found all the water boarders! add it to the total lands we come across.
+        # passed by reference, so nothing to return
         lands.update(thisIsland)
