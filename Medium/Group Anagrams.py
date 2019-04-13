670c6cf6f9f7b4c88449d37d8d7bee2293f28fce
Raymond Lam <raylam007@gmail.com>
2019-04-12 22:43:01 -0700

    49. Group Anagrams

---
+++ Medium/Group Anagrams.py
@@ -0,0 +1,19 @@
+'''
+https://leetcode.com/problems/group-anagrams/
+
+Given an array of strings, group anagrams together.
+Note:
+All inputs will be in lowercase.
+The order of your output does not matter.
+'''
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = dict()
+        for s in strs:
+            ssorted = str(sorted(s))
+            if ssorted in anagrams.keys():
+                anagrams[ssorted].append(s)
+            else:
+                anagrams[ssorted] = [s]
+        return list(anagrams.values())
