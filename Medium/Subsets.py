180d47ab9797429f675957216d21a09f7b942fa6
Raymond Lam <raylam007@gmail.com>
2019-04-15 02:30:18 -0700

    slight optimization

--- Medium/Subsets.py
+++ Medium/Subsets.py
@@ -8,23 +8,21 @@ Note: The solution set must not contain duplicate subsets.
 
 
 class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        results = list()
-        for sublen in range(len(nums)+1):
-            sub = list()
-            self.recurse(nums, 0, sublen, sub, results)
+	def subsets(self, nums: List[int]) -> List[List[int]]:
+		results = list()
+		for sublen in range(len(nums)+1):
+			self.recurse(nums, sublen, results)
+		return results
 
-        return results
+	def recurse(self, nums, sublen, results, index=0, sub=list()):
+		if len(sub) == sublen:
+			results.append(sub.copy())
+			return
 
+		remainingSpots = sublen - len(sub)
 
-    def recurse(self, nums, startI, sublen, sub, results):
-        if len(sub) == sublen:
-            results.append(sub.copy())
-            return
-
-        while startI < len(nums):
-            n = nums[startI]
-            sub.append(n)
-            self.recurse(nums, startI+1, sublen, sub, results)
-            sub.pop()
-            startI +=1
+		while index + remainingSpots <= len(nums):
+			sub.append(nums[index])
+			index +=1
+			self.recurse(nums, sublen, results, index, sub)
+			sub.pop()
