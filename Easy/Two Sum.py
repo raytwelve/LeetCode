edbc3c7611042424e38b88c193d2881eca89753f
Raymond Lam <raylam007@gmail.com>
2019-05-08 18:30:08 -0700

    updated 1. Two Sum

--- Easy/Two Sum.py
+++ Easy/Two Sum.py
@@ -5,14 +5,25 @@ Given an array of integers, return indices of the two numbers such that they add
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 '''
 
+# class Solution:
+# 	def twoSum(self, nums: List[int], target: int) -> List[int]:
+# 		ans = list()
+# 		sz = len(nums)-1
+# 		for i in range(sz):
+# 			num = nums[i]
+# 			com = target - num
+# 			if nums[i+1:].count(com) > 0:
+# 				ans.append(i)
+# 				ans.append(nums.index(com, i+1))
+# 				return ans
+
+# better solution
 class Solution:
-	def twoSum(self, nums: List[int], target: int) -> List[int]:
-		ans = list()
-		sz = len(nums)-1
-		for i in range(sz):
-			num = nums[i]
-			com = target - num
-			if nums[i+1:].count(com) > 0:
-				ans.append(i)
-				ans.append(nums.index(com, i+1))
-				return ans
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        compliments = dict()
+        for i in range(len(nums)):
+            x = nums[i]
+            c = target - x
+            if c in compliments.keys():
+                return [i, compliments[target-x]]
+            compliments[x] = i
