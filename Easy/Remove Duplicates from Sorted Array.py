f15a9ba7e93c27604ece91f3230e1f1feee8ee23
Raymond Lam <raylam007@gmail.com>
2019-05-09 09:57:35 -0700

    26. Remove Duplicates from Sorted Array; included a Java solution

--- Easy/Remove Duplicates from Sorted Array.py
+++ Easy/Remove Duplicates from Sorted Array.py
@@ -5,6 +5,7 @@ Given a sorted array nums, remove the duplicates in-place such that each element
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 '''
 
+# Python
 class Solution:
 	def removeDuplicates(self, nums: List[int]) -> int:
 		i = 0
@@ -13,3 +14,18 @@ class Solution:
 				nums.pop(i+1)
 			else:
 				i += 1
+
+
+# Java
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int l=1;
+        for(int i = 0; i<nums.length-1; i++) {
+            if (nums[i] != nums[i+1]) {
+                nums[l] = nums[i+1];
+                l++;
+            }
+        }
+        return l;
+    }
+}
