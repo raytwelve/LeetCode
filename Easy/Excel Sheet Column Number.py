77af600d6488fe2e4fc0bb3c052d71b733bdf830
Raymond Lam <raylam007@gmail.com>
2019-05-11 17:47:27 -0700

    171. Excel Sheet Column Number comments

--- Easy/Excel Sheet Column Number.py
+++ Easy/Excel Sheet Column Number.py
@@ -5,10 +5,14 @@ Given a column title as appear in an Excel sheet, return its corresponding colum
 '''
 
 class Solution:
-    def titleToNumber(self, s: str) -> int:
-        sum = 0
-        for i in range(len(s)):
-            # read backwards
-            ch = s[-(i+1)]
-            sum += (ord(ch) - ord('A') + 1) * 26**i
-        return sum
+	def titleToNumber(self, s: str) -> int:
+		sum = 0
+		for i in range(len(s)):
+			# in python, we can iterate with negative indices, which starts with the last item as -1, second to last as -2 ... etc
+			
+			# therefore, -1 == len(s)-1
+			
+			# using this info, we can manipulate our range of i = [0, len(s)-1] --> [-1, -len(s)]
+			ch = s[-(i+1)]
+			sum += (ord(ch) - ord('A') + 1) * 26**i
+		return sum
