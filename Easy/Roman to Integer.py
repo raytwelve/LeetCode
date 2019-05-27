cb80cb7ee0c1a136794d521523de7f5413b8f70f
Raymond Lam <raylam007@gmail.com>
2019-05-27 00:37:34 -0700

    13. Roman to Integer

--- Easy/Roman to Integer.py
+++ Easy/Roman to Integer.py
@@ -23,21 +23,21 @@ Given a roman numeral, convert it to an integer. Input is guaranteed to be withi
 '''
 
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        roman = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-        total = roman[s[0]]
-        for i in range(1, len(s)):
-            curr = roman[s[i]]
-            prev = roman[s[i-1]]
-            total += curr
-            if prev < curr:
-                total -= 2*prev
-        return total
+	def romanToInt(self, s: str) -> int:
+		roman = {
+			'I': 1,
+			'V': 5,
+			'X': 10,
+			'L': 50,
+			'C': 100,
+			'D': 500,
+			'M': 1000
+		}
+		total = roman[s[0]]
+		for i in range(1, len(s)):
+			curr = roman[s[i]]
+			prev = roman[s[i-1]]
+			total += curr
+			if prev < curr:
+				total -= 2*prev
+		return total
