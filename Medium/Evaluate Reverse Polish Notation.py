eda6d1c2b3594e06ff7c801bc75b7e95b27c5f15
Raymond Lam <raylam007@gmail.com>
2019-05-31 20:22:44 -0700

    150. Evaluate Reverse Polish Notation

---
+++ Medium/Evaluate Reverse Polish Notation.py
@@ -0,0 +1,36 @@
+'''
+https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+Note:
+Division between two integers should truncate toward zero.
+The given RPN expression is always valid. That means the expression would always evaluate to a result and there won't be any divide by zero operation.
+'''
+
+class Solution:
+	def evalRPN(self, tokens: List[str]) -> int:
+		stack = list()
+		ops = ['+', '-', '*', '/']
+		for i in range(len(tokens)):
+			item = tokens[i]
+			# print((item, stack))
+			if item in ops:
+				stack.append(self.operate(item, stack))
+			else:
+				stack.append(int(item))
+		return stack.pop()
+				
+				
+	def operate(self, op, terms):
+		y = terms.pop()
+		x = terms.pop()
+		if op == '+':
+			return x + y
+		elif op == '-':
+			return x-y
+		elif op == '*':
+			return x*y
+		elif op == '/':
+			return int(x/y)
