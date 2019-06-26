90f5769278119fe424a43b7f2abcc24655b285b9
Raymond Lam <raylam007@gmail.com>
2019-06-26 16:30:19 -0700

    34. Find First and Last Position of Element in Sorted Array

--- Medium/Find First and Last Position of Element in Sorted Array.py
+++ Medium/Find First and Last Position of Element in Sorted Array.py
@@ -1,38 +1,36 @@
-''''
+'''
 https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 
 Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+
 Your algorithm's runtime complexity must be in the order of O(log n).
 If the target is not found in the array, return [-1, -1].
 '''
 
+
 class Solution:
 	def searchRange(self, nums: List[int], target: int) -> List[int]:
-		# if not nums: return None
-		hi = len(nums)-1
-		lo = 0
-		mid = (lo+hi)//2
+		low = 0
+		high = len(nums)-1
 		
-		while lo <= hi:
-			mid = (lo+hi)//2
-			if target == nums[mid]:
-				left = self.seekLeft(nums, mid)
-				right = self.seekRight(nums, mid)
-				return [left, right]
-			elif target < nums[mid]:
-				hi = mid-1
-			elif target > nums[mid]:
-				lo = mid+1
-		return [-1, -1]
-	
-	
-	def seekLeft(self, nums, mid):
-		target = nums[mid]
-		while mid > -1 and nums[mid] == target:
-			mid -= 1
-		return mid+1
-	def seekRight(self, nums, mid):
-		target = nums[mid]
-		while mid < len(nums) and nums[mid] == target:
-			mid += 1
-		return mid-1
+		return self.find(nums, low, high, target)
+		
+	def find(self, nums, low, high, target):
+		if low > high: return [-1, -1]
+		mid = (low + high)//2
+		if target == nums[mid]:
+			start = self.getBound(nums, target, mid, low-1, -1)
+			end = self.getBound(nums, target, mid, high+1, 1)
+			return [start, end]
+		elif target > nums[mid]:
+			return self.find(nums, mid+1, high, target)
+		elif target < nums[mid]:
+			return self.find(nums, low, mid-1, target)
+			
+			
+	def getBound(self, nums, target, start, end, inc):
+		last = -1
+		for i in range(start, end, inc):
+			if target != nums[i]: break
+			last = i
+		return last
